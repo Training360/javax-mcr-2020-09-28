75776189b4b77e565f2048b8b08829ebd2a9abec
iviczian <viczian.istvan@gmail.com>
2020-09-28 16:45:43 +0200

    Find by id

--- employees/src/main/java/training/employees/EmployeesService.java
+++ employees/src/main/java/training/employees/EmployeesService.java
@@ -9,6 +9,8 @@ import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @AllArgsConstructor
@@ -24,9 +26,22 @@ public class EmployeesService {
                     ))
             );
 
-    public List<EmployeeDto> listEmployees() {
+    public List<EmployeeDto> listEmployees(Optional<String> prefix) {
+        var filtered = employees.stream()
+                .filter(e -> prefix.isEmpty() || e.getName().toLowerCase().startsWith(prefix.get().toLowerCase()))
+                .collect(Collectors.toList());
+
         Type targetListType = new TypeToken<List<EmployeeDto>>() {}.getType();
-        List<EmployeeDto> dtos = modelMapper.map(employees, targetListType);
+        List<EmployeeDto> dtos = modelMapper.map(filtered, targetListType);
         return dtos;
+    }
+
+    public EmployeeDto findEmployeeById(long id) {
+        var employee = employees.stream()
+                .filter(e -> e.getId() == id)
+                .findAny()
+                .orElseThrow(() -> new IllegalArgumentException("Employee not found: " + id));
+
+        return modelMapper.map(employee, EmployeeDto.class);
     }
 }
