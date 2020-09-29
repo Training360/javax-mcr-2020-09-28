e7e5e047cc4de68ca7a2f6a65e6fdfdd45bae1cd
iviczian <viczian.istvan@gmail.com>
2020-09-29 09:57:33 +0200

    Problem

--- employees/src/main/java/training/employees/EmployeesController.java
+++ employees/src/main/java/training/employees/EmployeesController.java
@@ -1,8 +1,14 @@
 package training.employees;
 
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.zalando.problem.Problem;
+import org.zalando.problem.Status;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -24,6 +30,7 @@ public class EmployeesController {
     }
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public EmployeeDto createEmployee(@RequestBody CreateEmployeeCommand command) {
         return employeesService.createEmployee(command);
     }
@@ -35,7 +42,22 @@ public class EmployeesController {
     }
 
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void deleteEmployee(@PathVariable long id) {
         employeesService.deleteEmployee(id);
+    }
+
+    @ExceptionHandler({IllegalArgumentException.class})
+    public ResponseEntity<Problem> handleNotFound(IllegalArgumentException  e) {
+        Problem problem = Problem.builder()
+                .withType(URI.create("employees/employee-not-found"))
+                .withTitle("Not found")
+                .withStatus(Status.NOT_FOUND)
+                .withDetail(e.getMessage())
+                .build();
+        return ResponseEntity
+                .status(HttpStatus.NOT_FOUND)
+                .contentType(MediaType.APPLICATION_PROBLEM_JSON)
+                .body(problem);
     }
 }
